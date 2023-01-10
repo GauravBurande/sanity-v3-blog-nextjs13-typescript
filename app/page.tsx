2fc61de79dcf58986c6f09a25f230b7521f0b519
Gaurav Burande <100035026+GauravBurande@users.noreply.github.com>
2023-01-10 19:25:02 +0530

    setting up sanity studio

---
+++ app/page.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const Page = () => {
+    return (
+        <div>
+            <h1 className='text-4xl'>welcome to the blog</h1>
+            <p>some content</p>
+        </div>
+    )
+}
+
+export default Page
---
+++ nodejs13-sanity-blog/sanity.cli.ts
@@ -0,0 +1,8 @@
+import {defineCliConfig} from 'sanity/cli'
+
+export default defineCliConfig({
+  api: {
+    projectId: '38qojq7u',
+    dataset: 'production'
+  }
+})
