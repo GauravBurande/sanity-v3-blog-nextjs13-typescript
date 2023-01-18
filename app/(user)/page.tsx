8bb523e24568a0a1e52ca0b08fa994244eb8359e
Gaurav Burande <100035026+GauravBurande@users.noreply.github.com>
2023-01-18 11:15:44 +0530

    ...........

--- app/(user)/page.tsx
+++ app/(user)/page.tsx
@@ -16,43 +16,46 @@ const query = groq`
 
 const Page = async () => {
 
-    // const PreviewBlogList = lazy(() => import("../../components/PreviewBlogList"))
+  // const PreviewBlogList = lazy(() => import("../../components/PreviewBlogList"))
 
-    if (previewData()) {
+  if (previewData()) {
 
-        // I'm having an error below here (index.js?cc3a:10 Uncaught TypeError: wakeable.then is not a function at Object.markComponentSuspended)
-        // I think it's related to the previewsuspense component or something related to the fallback, I've tried possibly every solution available on stackoverflow
-        // maybe the component wrapped in previewsuspense is not returning a promise
-        // there's no problem with this component, actually the React development tools extension in the browser causes problems with rendering components with nextJs13
+    // I'm having an error below here (index.js?cc3a:10 Uncaught TypeError: wakeable.then is not a function at Object.markComponentSuspended)
+    // I think it's related to the previewsuspense component or something related to the fallback, I've tried possibly every solution available on stackoverflow
+    // maybe the component wrapped in previewsuspense is not returning a promise
+    // there's no problem with this component, actually the React development tools extension in the browser causes problems with rendering components with nextJs13
 
-        return (
-            < PreviewSuspense
-            fallback={
-            <div className="text-center text-lg animate-pulse text-[#0ACBCB]">Loading Preview Data...</div>
-        }>
-                <PreviewBlogList query={query}/>
-        </PreviewSuspense>
-        )
-
-//1. One common cause of this error is that the fallback prop passed to the Suspense component is not a valid React element. Make sure that the fallback prop is a valid React element or a functional component that returns a valid React element.
-
-//2. Make sure that the component wrapped by the Suspense component is correctly imported and exported.
-
-//3. Another common issue is that the component wrapped by the Suspense component doesn't return a promise. Make sure that the component wrapped by Suspense component returns a promise.
-
-//4. Make sure that the component wrapped by the Suspense component is wrapped with the lazy() function, this is important to make sure the component is loaded on demand.
-
-//5. Make sure that you are using the correct version of React and React-DOM.
-
-//6. Make sure that you are using the correct version of next-react-dev and next-react-server
-}
-
-    const posts = await client.fetch(query)
     return (
-        <>
-        <BlogList posts={posts} />
-        </>
+      < PreviewSuspense
+        fallback={
+          <div className="text-center text-lg animate-pulse text-[#0ACBCB]">Loading Preview Data...</div>
+        }>
+        <PreviewBlogList query={query} />
+      </PreviewSuspense>
     )
+
+    //1. One common cause of this error is that the fallback prop passed to the Suspense component is not a valid React element. Make sure that the fallback prop is a valid React element or a functional component that returns a valid React element.
+
+    //2. Make sure that the component wrapped by the Suspense component is correctly imported and exported.
+
+    //3. Another common issue is that the component wrapped by the Suspense component doesn't return a promise. Make sure that the component wrapped by Suspense component returns a promise.
+
+    //4. Make sure that the component wrapped by the Suspense component is wrapped with the lazy() function, this is important to make sure the component is loaded on demand.
+
+    //5. Make sure that you are using the correct version of React and React-DOM.
+
+    //6. Make sure that you are using the correct version of next-react-dev and next-react-server
+  }
+
+  const posts = await client.fetch(query)
+  return (
+    <>
+      <BlogList posts={posts} />
+      <div className="flex items-center justify-center py-10">
+        <p> Built with <span className="text-[red]">❤</span> by <span className="hover:text-[#0acbcb] underline hover:decoration-white decoration-[#0acbcb] cursor-pointer"><a href="https://twitter.com/gauravvan">@gauravvan</a></span></p>
+      </div>
+    </>
+  )
 }
 
 export default Page
