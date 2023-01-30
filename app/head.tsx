56ca09bb09c90237fac9833c81a10a5a549bff30
Gaurav Burande <100035026+GauravBurande@users.noreply.github.com>
2023-01-30 18:17:04 +0530

    Update head.tsx

--- app/head.tsx
+++ app/head.tsx
@@ -4,6 +4,23 @@ export default function Head() {
       <title>CapTon's Blog</title>
       <meta content="width=device-width, initial-scale=1" name="viewport" />
       <link rel="icon" href="/favicon.ico" />
+      
+      <meta
+      name="description"
+      content="tech blog built using sanity and nexsjs."
+    />
+      
+      <meta
+      name="keywords"
+      content="blog , tech blog, html, css, javascript, reactjs, nextjs, tailwindcss, firebase, nodejs, mongodb, express"
+    />
+      
+      <!-- Twitter -->
+    <meta property="twitter:card" content="Ur CapTon's Blog"/>
+    <meta property="twitter:url" content="https://lnkd.in/gPYk-Eab/">
+    <meta property="twitter:title" content="read tech related blogs"/>
+    <meta property="twitter:description" content="read tech related blogs"/>
+    <meta property="twitter:image" content="https://github.com/GauravBurande/sanity-v3-blog-nextjs13-typescript/blob/main/public/Capton1.png"/>
     </>
   )
 }
